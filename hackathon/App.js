316a2360062240d543ed6f41ea87068960414e4c
leungt30 <leungt30@mcmaster.ca>
2024-02-03 17:28:47 -0500

    added firebase getter functionality

--- hackathon/App.js
+++ hackathon/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MyComponent from './components/test.js';
+import SingleEntity from './components/singleEntity.js';
 export default function App() {
   return (
     
     <View style={styles.container}>
-      <MyComponent text="hi"></MyComponent>
+      <SingleEntity id="gysZ6kEQKuc4P06GohFI"></SingleEntity>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>

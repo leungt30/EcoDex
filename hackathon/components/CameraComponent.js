f27583b6606828e86c15087ccd6f6d373b56a376
Bani Somo <banisomo2003@gmail.com>
2024-02-03 20:09:35 -0500

    merge conflicts?

--- hackathon/components/CameraComponent.js
+++ hackathon/components/CameraComponent.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
 
 const CameraComponent = () => {
